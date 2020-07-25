371c270bfb1221cdf0b1beb3fd980604964d925d
Catafcr2 <65142699+Catafcr2@users.noreply.github.com>
2020-07-25 14:25:36 -0400

    Add files via upload

---
+++ ServicioModulo/App_Code/ServicioModulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+public class ServicioModulo : IServicioModulo
+{
+	public int calcularModulo(int e, int i)
+	{
+		if (Math.Abs(e) >= Math.Abs(i))
+		{
+			if(i == 0)
+			{
+				return -1;
+			}
+			else
+			{
+				return e % i;
+			}
+		}
+		else
+		{
+			if(e == 0)
+			{
+				return -1;
+			}
+			else
+			{
+				return i % e;
+			}
+		}
+	}
+
+}
