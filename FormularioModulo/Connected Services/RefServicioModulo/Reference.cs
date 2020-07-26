93de8848efdbef42bdfbc54260c33c8abecdd6b0
Catafcr2 <65142699+Catafcr2@users.noreply.github.com>
2020-07-25 21:25:43 -0400

    Update Reference.cs

--- FormularioModulo/Connected Services/RefServicioModulo/Reference.cs
+++ FormularioModulo/Connected Services/RefServicioModulo/Reference.cs
@@ -11,23 +11,23 @@
 namespace FormularioModulo.RefServicioModulo {
     
     
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
     [System.ServiceModel.ServiceContractAttribute(ConfigurationName="RefServicioModulo.IServicioModulo")]
     public interface IServicioModulo {
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IServicioModulo/calcularModulo", ReplyAction="http://tempuri.org/IServicioModulo/calcularModuloResponse")]
-        int calcularModulo(int a, int b);
+        int calcularModulo(int e, int i);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IServicioModulo/calcularModulo", ReplyAction="http://tempuri.org/IServicioModulo/calcularModuloResponse")]
-        System.Threading.Tasks.Task<int> calcularModuloAsync(int a, int b);
+        System.Threading.Tasks.Task<int> calcularModuloAsync(int e, int i);
     }
     
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
     public interface IServicioModuloChannel : FormularioModulo.RefServicioModulo.IServicioModulo, System.ServiceModel.IClientChannel {
     }
     
     [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "3.0.0.0")]
     public partial class ServicioModuloClient : System.ServiceModel.ClientBase<FormularioModulo.RefServicioModulo.IServicioModulo>, FormularioModulo.RefServicioModulo.IServicioModulo {
         
         public ServicioModuloClient() {
@@ -49,12 +49,12 @@ namespace FormularioModulo.RefServicioModulo {
                 base(binding, remoteAddress) {
         }
         
-        public int calcularModulo(int a, int b) {
-            return base.Channel.calcularModulo(a, b);
+        public int calcularModulo(int e, int i) {
+            return base.Channel.calcularModulo(e, i);
         }
         
-        public System.Threading.Tasks.Task<int> calcularModuloAsync(int a, int b) {
-            return base.Channel.calcularModuloAsync(a, b);
+        public System.Threading.Tasks.Task<int> calcularModuloAsync(int e, int i) {
+            return base.Channel.calcularModuloAsync(e, i);
         }
     }
 }
